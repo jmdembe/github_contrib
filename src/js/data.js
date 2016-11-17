a2e4b967c783dcc1995510ef3ab71adad122e705
Jessica Dembe <jmdembe@gmail.com>
2016-11-17 16:07:47 -0500

    Created first call for results

--- src/js/data.js
+++ src/js/data.js
@@ -1,4 +1,24 @@
 (function() {
     'use strict';
-    
+
+    window.ghContrib = window.ghContrib || {};
+
+    window.ghContrib.firstCall = firstCall;
+
+    var token;
+
+    /**
+     * Activates the first Ajax call
+     * @return {Promise} [description]
+     */
+    function firstCall(token) {
+        return $.ajax({
+            url: 'https://api.github.com/search/repositories',
+            method: 'GET',
+            dataType: 'json',
+            headers: {
+                Authorization: 'token '+ token
+            }
+        })
+    }
 })();
