45b57bd942de61a651c48131cc6eb5bb79ceb4b2
Jessica Dembe <jmdembe@gmail.com>
2016-11-17 15:34:31 -0500

    added grunt jshint to automation process

---
+++ gruntfile.js
@@ -0,0 +1,28 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+
+        // task name
+        //    options
+        //    targetname
+        //        options
+        //    targetname
+        //        options
+
+        jshint: {
+            options: {
+              jshintrc: '.jshintrc'
+            },
+            all: {
+              files: {
+                src:[
+                  'src/**/*.js',
+                  'test/**/*.js'
+                ]
+              }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+}
